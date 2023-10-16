fab8df8b83d8d674e97385005d8c7f70f6082e18
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-16 11:11:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.If_number_contains_3
+{
+    internal class Program
+    {
+        public static bool IfNumberContains3(int number)
+        {
+            while (number > 0)
+            {
+                if (number % 10 == 3)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+
+        }
+        public static void Main()
+        {
+            Console.WriteLine(IfNumberContains3(7201432));   
+            Console.WriteLine(IfNumberContains3(87501));
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
+
